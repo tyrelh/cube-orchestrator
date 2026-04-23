1f34296a79a562ea90b56b3f0417b49975748435
Tyrel Delaney <tyrel.hiebert@gmail.com>
2026-04-23 16:20:20 -0700

    implement the Task.Run() function

--- go.mod
+++ go.mod
@@ -3,7 +3,30 @@ module cube
 go 1.26.1
 
 require (
-	github.com/docker/go-connections v0.7.0
 	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
 	github.com/google/uuid v1.6.0
+	github.com/moby/moby/api v1.52.0
+	github.com/moby/moby/client v0.1.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.7.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- task/task.go
+++ task/task.go
@@ -1,10 +1,20 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"math"
+	"os"
 	"time"
 
-	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
+
+	// needed to modify docker imports from github.com/docker/docker to github.com/moby/moby
+	"github.com/moby/moby/api/pkg/stdcopy"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/network"
+	"github.com/moby/moby/client"
 )
 
 type State int
@@ -18,13 +28,14 @@ const (
 )
 
 type Task struct {
-	ID            uuid.UUID
-	Name          string
-	State         State
-	Image         string
-	Memory        int
-	Disk          int
-	ExposedPorts  nat.PortSet
+	ID     uuid.UUID
+	Name   string
+	State  State
+	Image  string
+	Memory int
+	Disk   int
+	// needed to modify nat.PortSet to network.PortSet
+	ExposedPorts  network.PortSet
 	PortBindings  map[string]string
 	RestartPolicy string
 	StartTime     time.Time
@@ -36,4 +47,108 @@ type TaskEvent struct {
 	State     State
 	Timestamp time.Time
 	Task      Task
+}
+
+type Config struct {
+	Name         string
+	AttachStdin  bool
+	AttachStdout bool
+	AttachStderr bool
+	// needed to modify nat.PortSet to network.PortSet
+	ExposedPorts network.PortSet
+	Cmd          []string
+	Image        string
+	Cpu          float64
+	Memory       int64
+	Disk         int64
+	Env          []string
+	// needed to modify RestartPolicy from a string to container.RestartPolicyMode
+	RestartPolicy container.RestartPolicyMode
+}
+
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	// needed to modify types.ImagePullOptions to client.ImagePullOptions
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, client.ImagePullOptions{})
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+
+	restartPolicy := container.RestartPolicy{
+		Name: d.Config.RestartPolicy,
+	}
+	resources := container.Resources{
+		Memory:   d.Config.Memory,
+		NanoCPUs: int64(d.Config.Cpu * math.Pow(10, 9)),
+	}
+	containerConfig := container.Config{
+		Image:        d.Config.Image,
+		Tty:          false,
+		Env:          d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+	hostConfig := container.HostConfig{
+		RestartPolicy:   restartPolicy,
+		Resources:       resources,
+		PublishAllPorts: true, // automaticall map internal ports to host ports
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, client.ContainerCreateOptions{
+		Config:           &containerConfig,
+		HostConfig:       &hostConfig,
+		NetworkingConfig: nil,
+		Platform:         nil,
+		Name:             d.Config.Name,
+		Image:            d.Config.Image,
+	})
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	// needed to ignore client.ContainerStartResult return. Maybe act on it?
+	_, err = d.Client.ContainerStart(ctx, resp.ID, client.ContainerStartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	// commented this out for now as d.Config.Runtime doesn't exist
+	// d.Config.Runtime.ContainerID = resp.ID
+
+	out, err := d.Client.ContainerLogs(
+		ctx,
+		resp.ID,
+		client.ContainerLogsOptions{
+			ShowStdout: true,
+			ShowStderr: true,
+		},
+	)
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return DockerResult{
+		ContainerId: resp.ID,
+		Action:      "start",
+		Result:      "success",
+	}
+
 }
