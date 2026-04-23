c72a515cd64e2ee7c1f7858d7981101ba51d2ffe
Tyrel Delaney <tyrel.hiebert@gmail.com>
2026-04-23 12:41:51 -0700

    initial task struct

---
+++ task/task.go
@@ -0,0 +1,21 @@
+package task
+
+import {
+	"github.com/google/uuid"
+}
+
+type State int
+
+const {
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+}
+
+type Task struct {
+	ID    uuid.UUID
+	Name  string
+	State State
+}
