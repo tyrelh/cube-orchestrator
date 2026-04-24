c486cb40eea4f88b94dc758ed1f71194f8a0f018
Tyrel Delaney <tyrel.hiebert@gmail.com>
2026-04-24 09:25:26 -0700

    testing creating and stopping a container/task

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"cube/task"
 	"cube/worker"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"github.com/moby/moby/client"
 )
 
 func main() {
@@ -64,4 +66,51 @@ func main() {
 		Role:   "worker",
 	}
 	fmt.Printf("node: %v\n", n)
+
+	// testing starting and stopping a container
+
+	fmt.Print("create a test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
+	}
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask, createResult.ContainerId)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	config := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:18",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	// book note: needed to add client.WithAPIVersionNegotiation()
+	dockerClient, _ := client.New(client.FromEnv, client.WithAPIVersionNegotiation())
+	dockerTask := task.Docker{
+		Client: dockerClient,
+		Config: config,
+	}
+
+	result := dockerTask.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, &result
+	}
+	return &dockerTask, &result
+}
+
+func stopContainer(dockerTask *task.Docker, id string) *task.DockerResult {
+	result := dockerTask.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
