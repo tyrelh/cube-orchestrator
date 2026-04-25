41e13aa805a22e6350d2d646e8fa729cb8d19a74
Tyrel Delaney <tyrel.hiebert@gmail.com>
2026-04-24 20:33:47 -0700

    StopTask & StartTask

--- worker/worker.go
+++ worker/worker.go
@@ -2,6 +2,8 @@ package worker
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -20,14 +22,44 @@ func (w *Worker) CollectStats() {
 	fmt.Println("I will collect stats")
 }
 
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
+}
+
 func (w *Worker) RunTask() {
 	fmt.Println("I will start or stop a task")
 }
 
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	docker := task.NewDocker(config)
+	result := docker.Run()
+	if result.Error != nil {
+		log.Printf("Error running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+	return result
 }
 
-func (w *Worker) StopTask() {
-	fmt.Println("I will stop a task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	docker := task.NewDocker(config)
+
+	result := docker.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stopping container %s: %v\n", t.ContainerID, result.Error)
+		// book note: added missing return
+		return task.DockerResult{Error: result.Error}
+	}
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+	log.Printf("Stopped and removed container %s for task %v\n", t.ContainerID, t.ID)
+	return result
 }
