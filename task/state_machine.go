2e8b2fe2df55ab1bfae189718bfe3d7cad67a17b
Tyrel Delaney <tyrel.hiebert@gmail.com>
2026-04-24 20:33:01 -0700

    implement task state transitions

---
+++ task/state_machine.go
@@ -0,0 +1,30 @@
+package task
+
+import "slices"
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+var stateTransitionMap = map[State][]State{
+	Pending:   {Scheduled},
+	Scheduled: {Scheduled, Running, Failed},
+	Running:   {Running, Completed, Failed},
+	Completed: {},
+	Failed:    {},
+}
+
+// func Contains(states []State, state State) bool {
+// 	return slices.Contains(states, state)
+// }
+
+func ValidStateTransition(src State, dst State) bool {
+	// return Contains(stateTransitionMap[src], dst)
+	return slices.Contains(stateTransitionMap[src], dst)
+}
